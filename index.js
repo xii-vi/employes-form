59770de5b35b29ac1ab4e3f8c387350a588a3635
xii-vi <rohitabhishek318@gmail.com>
2021-08-04 09:27:55 +0530

    basic form added

---
+++ index.js
@@ -0,0 +1,11 @@
+var empName = document.querySelector("#empName");
+var empAge = document.querySelector("#empAge");
+var empCountry = document.querySelector("#country");
+var empState = document.querySelector("#state");
+var submitBtn = document.querySelector("#save");
+
+function clickEventHandler() {
+
+}
+
+submitBtn.addEventListener(click,clickEventHandler);
