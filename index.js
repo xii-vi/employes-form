7723faec2572a5d405e8684df01b11a7aca50972
xii-vi <rohitabhishek318@gmail.com>
2021-08-04 10:42:33 +0530

    added tables

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var empAge = document.querySelector("#empAge");
 var empCountry = document.querySelector("#country");
 var empState = document.querySelector("#state");
 var submitBtn = document.querySelector("#save");
+var tableUser = document.querySelector("#table1");
 
 function clickEventHandler() {
 
